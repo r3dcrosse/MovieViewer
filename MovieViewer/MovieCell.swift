ad049731a6e7bb3f251dd894fc55430a81811742
r3dcrosse <mrdavidwayman@gmail.com>
2016-01-19 17:14:05 -0800

    Added movie poster images
    Also used AFNetworking and Cocoapods.

--- MovieViewer/MovieCell.swift
+++ MovieViewer/MovieCell.swift
@@ -12,6 +12,7 @@ class MovieCell: UITableViewCell {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
     
     
     override func awakeFromNib() {
